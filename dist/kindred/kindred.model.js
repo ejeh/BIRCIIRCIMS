51b93c61cc7e02c902b67e273ee3c9a97dc2bc54
ejeh <ejehgodfrey@gmail.com>
2025-05-05 07:17:08 -0700

    Remove dist folder from version control and add to .gitignore

--- dist/kindred/kindred.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.KindredModel = void 0;
-const mongoose_1 = require("@nestjs/mongoose");
-const kindred_schema_1 = require("./kindred.schema");
-exports.KindredModel = mongoose_1.MongooseModule.forFeature([
-    { name: 'Kindred', schema: kindred_schema_1.KindredSchema },
-]);
-//# sourceMappingURL=kindred.model.js.map
--- dist/users/parse-json.pipe.d.ts
+++
@@ -1,4 +0,0 @@
-import { ArgumentMetadata, PipeTransform } from '@nestjs/common';
-export declare class ParseJSONPipe implements PipeTransform {
-    transform(value: any, metadata: ArgumentMetadata): any;
-}
