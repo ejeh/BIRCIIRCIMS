bab8238609aec6df7ab46b47df35bb0883615a68
ejeh <ejehgodfrey@gmail.com>
2025-05-30 15:12:41 -0700

    Stop tracking dist/ directory

--- dist/kindred/kindred.model.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const KindredModel: import("@nestjs/common").DynamicModule;
--- dist/users/users.mailer.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { MailerService } from '@nest-modules/mailer';
-export declare class UserMailerService {
-    private readonly mailerService;
-    constructor(mailerService: MailerService);
-    sendActivationMail(email: string, userId: string, activationToken: string, origin: string): void;
-    sendForgottenPasswordMail(email: string, passwordResetToken: string, origin: string): void;
-    sendResetPasswordMail(email: string): void;
-    sendMailRequest(email: string, subject: string, body: string): void;
-}
